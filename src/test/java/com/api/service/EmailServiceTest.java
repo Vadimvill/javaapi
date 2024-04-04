448363121586fa8ae3e85c4b448fd2a235a41d7f
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-04-04 22:44:54 +0300

    Add files via upload

--- src/test/java/com/api/service/EmailServiceTest.java
+++ src/test/java/com/api/service/EmailServiceTest.java
@@ -19,6 +19,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
 
 class EmailServiceTest {
 
@@ -44,109 +47,138 @@ class EmailServiceTest {
 
     @Test
     void shouldReturnAllEmails() {
-        Mockito.when(emailRepository.findAll()).thenReturn(getEmails());
+        when(emailRepository.findAll()).thenReturn(getEmails());
         List<EmailDTO> res = emailService.getEmails("all");
-        Assertions.assertEquals(3,res.size());
+        Assertions.assertEquals(3, res.size());
         Assertions.assertNotNull(res.get(0));
     }
+
     @Test
     void shouldReturnProcessedText() {
         String text1 = "rooror@mail.com give+375292556867";
         String text2 = "rooror@mail.com give";
         String text3 = "give+375292556867";
         String text4 = "give";
-        Assertions.assertEquals(" give",emailService.getConfidentialText(text1));
-        Assertions.assertEquals(" give",emailService.getConfidentialText(text2));
-        Assertions.assertEquals("give",emailService.getConfidentialText(text3));
-        Assertions.assertEquals("give",emailService.getConfidentialText(text4));
+        Assertions.assertEquals(" give", emailService.getConfidentialText(text1));
+        Assertions.assertEquals(" give", emailService.getConfidentialText(text2));
+        Assertions.assertEquals("give", emailService.getConfidentialText(text3));
+        Assertions.assertEquals("give", emailService.getConfidentialText(text4));
     }
+
     @Test
-    void shouldReturnEmailsByEmailType(){
+    void shouldReturnEmailsByEmailType() {
         List<Email> list = new ArrayList<>();
-        list.add(new Email(1L,"gagagga@mail.ru"));
-        list.add(new Email(2L,"wqeqe@mail.ru"));
-        Mockito.when(emailRepository.findByEmailTypeDomain("mail.ru")).thenReturn(list);
+        list.add(new Email(1L, "gagagga@mail.ru"));
+        list.add(new Email(2L, "wqeqe@mail.ru"));
+        when(emailRepository.findByEmailTypeDomain("mail.ru")).thenReturn(list);
         List<EmailDTO> emailDTOList = new ArrayList<>();
         emailDTOList = emailService.getEmailsByEmailType("mail.ru");
         Assertions.assertNotNull(emailDTOList);
-        Assertions.assertEquals(emailDTOList.size(),2);
+        Assertions.assertEquals(emailDTOList.size(), 2);
     }
+
     @Test
-    void shouldReturnUpdateEmailsByName(){
+    void shouldReturnUpdateEmailsByName() {
         String oldEmail = "old@example.com";
         String newEmail = "new@example.com";
         Email emailEntity = new Email(oldEmail);
         EmailType emailType = new EmailType("example.com");
 
         // Mock repository behavior
-        Mockito.when(emailRepository.findByName(oldEmail)).thenReturn(emailEntity);
-        Mockito.when(emailTypeRepository.findByDomain("example.com")).thenReturn(emailType);
+        when(emailRepository.findByName(oldEmail)).thenReturn(emailEntity);
+        when(emailTypeRepository.findByDomain("example.com")).thenReturn(emailType);
 
         // Invoke method
         emailService.updateEmail(oldEmail, newEmail);
 
-        Mockito.verify(emailRepository,Mockito.times(1)).findByName(Mockito.any());
+        verify(emailRepository, Mockito.times(1)).findByName(Mockito.any());
     }
+
     @Test
-    void shouldReturnUpdateEmailsById(){
+    void shouldReturnUpdateEmailsById() {
         String oldEmail = "old@example.com";
         String newEmail = "new@example.com";
         EmailType emailType = new EmailType("example.com");
 
         // Mock repository behavior
-        Mockito.when(emailRepository.findById(1L)).thenReturn(Optional.of(new Email(1L,oldEmail)));
-        Mockito.when(emailTypeRepository.findByDomain("example.com")).thenReturn(emailType);
+        when(emailRepository.findById(1L)).thenReturn(Optional.of(new Email(1L, oldEmail)));
+        when(emailTypeRepository.findByDomain("example.com")).thenReturn(emailType);
 
         // Invoke method
         emailService.updateEmail(1L, newEmail);
 
-        Mockito.verify(emailRepository,Mockito.times(1)).findById(1L);
+        verify(emailRepository, Mockito.times(1)).findById(1L);
     }
+
     @Test
-    void shouldReturnUpdateEmailsByIdWithoutDomain(){
+    void shouldReturnUpdateEmailsByIdWithoutDomain() {
         String oldEmail = "old@example.com";
         String newEmail = "new@example.com";
         EmailType emailType = new EmailType("example.com");
 
         // Mock repository behavior
-        Mockito.when(emailRepository.findById(1L)).thenReturn(Optional.of(new Email(1L,oldEmail)));
-        Mockito.when(emailTypeRepository.findByDomain("example.com")).thenReturn(null);
+        when(emailRepository.findById(1L)).thenReturn(Optional.of(new Email(1L, oldEmail)));
+        when(emailTypeRepository.findByDomain("example.com")).thenReturn(null);
 
         // Invoke method
         emailService.updateEmail(1L, newEmail);
 
-        Mockito.verify(emailRepository,Mockito.times(1)).findById(1L);
+        verify(emailRepository, Mockito.times(1)).findById(1L);
     }
+
     @Test
-    void shouldReturnUpdateEmailsByNameWithoutDomain(){
+    void shouldReturnUpdateEmailsByNameWithoutDomain() {
         String oldEmail = "old@example.com";
         String newEmail = "new@example.com";
         Email emailEntity = new Email(oldEmail);
         EmailType emailType = new EmailType("example.com");
 
         // Mock repository behavior
-        Mockito.when(emailRepository.findByName(oldEmail)).thenReturn(emailEntity);
-        Mockito.when(emailTypeRepository.findByDomain("example.com")).thenReturn(null);
+        when(emailRepository.findByName(oldEmail)).thenReturn(emailEntity);
+        when(emailTypeRepository.findByDomain("example.com")).thenReturn(null);
 
         // Invoke method
         emailService.updateEmail(oldEmail, newEmail);
 
-        Mockito.verify(emailRepository,Mockito.times(1)).findByName(Mockito.any());
+        verify(emailRepository, Mockito.times(1)).findByName(Mockito.any());
     }
 
+    @Test
+    void testDeleteEmailEmailExistsSuccess() {
+        // Mock data
+        Long id = 1L;
+        String email = "test@example.com";
+        Email emailEntity = new Email(email);
 
-    @Transactional
-    public List<EmailDTO> getEmailsByEmailType(String text) {
-        List<EmailDTO> strings = new ArrayList<>();
-        List<Email> emailEntities;
-        emailEntities = emailRepository.findByEmailTypeDomain(text);
-        for (int i = 0; i < emailEntities.size(); i++) {
-            cache.put(emailEntities.get(i).getEmail(), emailEntities.get(i));
-            customLogger.logCachePut(emailEntities.get(i).getEmail());
-            strings.add(new EmailDTO(emailEntities.get(i).getId().toString() + ". " + emailEntities.get(i).getEmail()));
-        }
+        // Mock repository behavior
+        when(emailRepository.findById(id)).thenReturn(Optional.of(emailEntity));
 
-        return strings;
+        // Invoke method
+        emailService.deleteEmail(id);
+
+        // Verify interactions and assertions
+        verify(emailRepository).findById(id);
+        verify(cache).remove(email);
+        verify(customLogger).logCacheRemove(email);
+        verify(emailRepository).delete(emailEntity);
+    }
+
+    @Test
+    public void testDeleteEmailEmailNotFoundErrorLogged() {
+        // Mock data
+        Long id = 1L;
+
+        // Mock repository behavior
+        when(emailRepository.findById(id)).thenReturn(Optional.empty());
+
+        // Invoke method and verify exception
+        assertThrows(ServiceException.class, () -> emailService.deleteEmail(id));
+
+        // Verify interactions and assertions
+        verify(emailRepository).findById(id);
+        verify(customLogger).logError("Email is not found");
+        verifyNoMoreInteractions(cache);
+        verifyNoMoreInteractions(emailRepository);
     }
     private List<Email> getEmails(){
         List<Email> list = new ArrayList<>();
@@ -155,6 +187,7 @@ class EmailServiceTest {
         list.add(new Email(3L,"vafda@rambler.ru"));
         return list;
     }
+
 }
 
 
